68412eaa4bb405edec3e610105fd33d0d37917a1
Joanna Gajda <Asia@Asia.(none)>
2013-06-16 17:13:25 +0200

    RefPoint implementation

---
+++ src/RefPoints/NegativeValueException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package RefPoints;
+
+/**
+ *
+ * @author Asia
+ */
+public class NegativeValueException extends Exception {
+    public void showMessage() {
+        System.out.println("Value can not be negative");
+    }
+
+}
